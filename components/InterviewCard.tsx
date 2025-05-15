6d4013454fae9a05b2b5ff435d02f955b8a9bc09
Mike <binarybardcode@gmail.com>
2025-05-15 14:10:33 +0200

    feat: add feedback page for interview results with detailed analysis and user feedback retrieval

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,9 +4,12 @@ import Image from 'next/image';
 import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
+import { getFeedbackByInterviewId } from '@/lib/actions/general.actions';
 
-const InterviewCard = ({ id,  role, type, techstack, createdAt }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async ({ id, userId,  role, type, techstack, createdAt }: InterviewCardProps) => {
+    
+    const feedback = userId && id ? await getFeedbackByInterviewId({ interviewId: id, userId }) : null;
+    
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('DD/MM/YYYY');
   return (
